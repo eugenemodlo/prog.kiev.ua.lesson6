d696396b5a2fca36b8cc48becc2d0eef72108b65
eugenemodlo <eugenemodlo@gmail.com>
2022-06-29 14:47:18 +0300

    Added single thread array sum

--- prog/kiev/ua/lesson6/Main.java
+++ prog/kiev/ua/lesson6/Main.java
@@ -1,13 +1,26 @@
 package prog.kiev.ua.lesson6;
 
+import prog.kiev.ua.lesson6.math.ArraySumThread;
 import prog.kiev.ua.lesson6.math.MathFactorial;
 
 public class Main {
     public static void main(String[] args) {
+//         STEP 1
+//        for (int i = 1; i <= 100; i++) {
+//            MathFactorial factorial = new MathFactorial(i);
+//        }
 
-        for (int i = 1; i <= 100; i++) {
-            MathFactorial factorial = new MathFactorial(i);
+//        STEP 2
+        int[] input = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        ArraySumThread arraySumThread = new ArraySumThread(input, 0, 9);
+
+        try {
+            arraySumThread.getThread().join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
+        System.out.println(arraySumThread.getSum());
     }
 }
---
+++ prog/kiev/ua/lesson6/math/ArraySumThread.java
@@ -0,0 +1,36 @@
+package prog.kiev.ua.lesson6.math;
+
+public class ArraySumThread implements Runnable {
+    final private int[] inputArray;
+    private final int begin;
+    private final int end;
+    private double sum = 0;
+    private final Thread thread;
+
+    public ArraySumThread(int[] inputArray, int begin, int end) {
+        this.inputArray = inputArray;
+        this.begin = begin;
+        this.end = end;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = begin; i <= end; i++) {
+            if (i < inputArray.length) {
+                sum = sum + inputArray[i];
+            }
+
+        }
+    }
+
+    public double getSum() {
+        return this.sum;
+    }
+
+    public Thread getThread() {
+        return this.thread;
+    }
+
+}
